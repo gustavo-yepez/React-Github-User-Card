8d23147f85a7d77cbcc75fa3eb87d7cbfd7fe264
Gustavo Yepez <gustavo@Gustavos-MacBook-Pro.local>
2020-03-15 23:20:20 -0400

    MVP

---
+++ my-app/src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar() {
+    return (
+        <form>
+            <h2>Search User</h2>
+            <input type="text" />
+        </form>
+    )
+}
+
+export default SearchBar;
